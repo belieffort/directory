dae641f3cd3d0f8278147cc166faa2f61319fe19
한병두 <ByungdooHan@hanbyeongduui-macbook-pro.local>
2018-05-14 16:21:51 +0900

    Initial Commit

---
+++ homework2_movetopage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  homework2_movetopage
+//
+//  Created by 한병두 on 2018. 5. 14..
+//  Copyright © 2018년 Byungdoo Han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
